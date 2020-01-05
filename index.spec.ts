a11f39edbfebab1585d45636e2e90b72259af454
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-05 01:52:15 +0100

    add 3rd case test --perf --cov 💹 ..

--- index.spec.ts
+++ index.spec.ts
@@ -1,11 +1,12 @@
 import { expect } from 'chai';
 import { shortObjectId , MongoDB_ObjectId , UserConfig } from '.';
-
+import { __OID_LENGTH__ } from './config.json';
 
 
 describe('unit test using ` mocha ☕️ ` & ` chai 🍵 ` is running 👻 ..', () => {
 
-  let __MongoDB_ObjectID__: MongoDB_ObjectId , 
+  let __MongoDB_ObjectID__: MongoDB_ObjectId,
+  __MongoDB_ObjectID__CATCH__: MongoDB_ObjectId, 
   __Config__: UserConfig,
   __Result__: number,
   __Correct_Result__: number;
@@ -13,12 +14,13 @@ describe('unit test using ` mocha ☕️ ` & ` chai 🍵 ` is running 👻 ..', 
 
   beforeEach(() =>{
    __MongoDB_ObjectID__ = { "$oid" : "507f191e810c19729de860ea" };
+   __MongoDB_ObjectID__CATCH__ = { "$oid" : "507f191e810c19*29de860ea" }
    __Config__ = { Timestamp: 57, MachineId: 65, ProcessId: 48, Counter: 47 };
   })
 
 
   it('ideal sutation with all params 🚀 .. ', () => {
-     __Result__ = shortObjectId(__MongoDB_ObjectID__,__Config__);
+     __Result__ = shortObjectId(__MongoDB_ObjectID__, __Config__);
      __Correct_Result__ = 532860;
      expect(__Result__).to.equal(__Correct_Result__);
   });
@@ -28,6 +30,13 @@ describe('unit test using ` mocha ☕️ ` & ` chai 🍵 ` is running 👻 ..', 
      __Result__ = shortObjectId(__MongoDB_ObjectID__);
      __Correct_Result__ = 3591228;
      expect(__Result__).to.equal(__Correct_Result__);
+  });
+
+
+  it('catch case ⛔️ .. ', () => {
+   
+     expect(() => shortObjectId(__MongoDB_ObjectID__CATCH__)).to.throw(new Error('Please check your MongoDB ObjectId : the value of `$oid` should be have only lowercase character and number `<a..z> & <0..9>` ..').message);
+
   });
 
 });
